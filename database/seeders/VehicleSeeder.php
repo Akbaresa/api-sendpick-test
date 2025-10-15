f1a99d9ecc8e8e72159b576b9a9acf6a69d9946c
Akbaresa <takutoakbar354@gmail.com>
2025-10-15 11:42:44 +0700

    add: faker database seeder to table vehicle

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Vehicle;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,26 @@ class VehicleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $vehicles = [
+            [
+                'plate_number' => 'B 1234 SPK',
+                'type' => 'Truck Box',
+                'capacity' => 5000,
+            ],
+            [
+                'plate_number' => 'B 5678 SPD',
+                'type' => 'Pickup',
+                'capacity' => 2000,
+            ],
+            [
+                'plate_number' => 'L 4321 SPR',
+                'type' => 'Van',
+                'capacity' => 1500,
+            ],
+        ];
+
+        foreach ($vehicles as $vehicle) {
+            Vehicle::create($vehicle);
+        }
     }
 }
