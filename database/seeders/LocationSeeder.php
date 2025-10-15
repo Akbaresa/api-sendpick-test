9da6e09ada524ba8ce0d514c75c399d94835494d
Akbaresa <takutoakbar354@gmail.com>
2025-10-15 11:43:43 +0700

    add: faker database seeder to table location

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class LocationSeeder extends Seeder
 {
     /**
@@ -12,6 +12,26 @@ class LocationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create('id_ID');
+
+        $jobOrders = JobOrder::all();
+
+        foreach ($jobOrders as $jobOrder) {
+            Location::create([
+                'job_order_id' => $jobOrder->id_job_order,
+                'type' => 'pickup',
+                'address' => $jobOrder->pickup_address,
+                'lat' => $faker->latitude(-6.4, -6.9),
+                'lng' => $faker->longitude(106.7, 107.1),
+            ]);
+
+            Location::create([
+                'job_order_id' => $jobOrder->id_job_order,
+                'type' => 'destination',
+                'address' => $jobOrder->destination_address,
+                'lat' => $faker->latitude(-7.0, -6.3),
+                'lng' => $faker->longitude(106.6, 107.2),
+            ]);
+        }
     }
 }
