349c43f519d09717a7d1151cd52d0ecb48af33ef
Akbaresa <takutoakbar354@gmail.com>
2025-10-15 11:43:31 +0700

    add: faker database seeder to table manifest

--- database/seeders/ManifestSeeder.php
+++ database/seeders/ManifestSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\JobOrder;
+use App\Models\Manifest;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,23 @@ class ManifestSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = \Faker\Factory::create('id_ID');
+
+        $jobOrders = JobOrder::all();
+
+        foreach ($jobOrders as $jobOrder) {
+            $manifestCount = rand(2, 5);
+
+            for ($i = 0; $i < $manifestCount; $i++) {
+                Manifest::create([
+                    'job_order_id' => $jobOrder->id_job_order,
+                    'item_name' => $faker->words(rand(1, 3), true),
+                    'quantity' => $faker->numberBetween(1, 20),
+                    'weight' => $faker->randomFloat(2, 1, 500),
+                    'volume' => $faker->randomFloat(2, 0.5, 50),
+                    'notes' => $faker->boolean(30) ? $faker->sentence() : null,
+                ]);
+            }
+        }
     }
 }
